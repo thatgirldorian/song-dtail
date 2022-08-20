237a7e7a5dcf4de95535ccb5f899a1a20ed2c524
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-20 13:38:12 +0400

    Added the basic components

---
+++ src/components/SongDetail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SongDetail = () => {
+    return (
+        <div>The songdetail component</div>
+    )
+}
+
+export default SongDetail;
---
+++ src/components/SongList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SongList = () => {
+    return (
+        <div>The song list component</div>
+    )
+}
+
+export default SongList
