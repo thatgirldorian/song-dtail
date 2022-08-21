4bdf435e02997b2a7a9940ab8968d5ca73558172
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-21 23:32:54 +0400

    Added some song content

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import SongList from './SongList'
 import SongDetail from './SongDetail'
-import { selectSong } from '../actions'
 
 const App = () => {
 
   return (
-    <div>
-      <SongList />
-      <SongDetail />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   )
 }
