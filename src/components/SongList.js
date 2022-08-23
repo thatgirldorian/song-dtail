2214a40451a2df38e21aa7d4b04b7318ed3683b5
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-08-23 21:33:29 +0400

    called action creators from components

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectSong } from '../actions'
 
 class SongList extends Component {
     //create a helper method to return the list of songs
@@ -8,7 +9,9 @@ class SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button className="ui button primary"
+                                onClick={() => this.props.selectSong(song)}
+                        >Select</button>
                     </div>
                     <div className="content">{song.title}</div>
                 </div>
@@ -27,7 +30,8 @@ class SongList extends Component {
 
 //create an instance of the connect component and use state from the redux store
 const mapStateToProps = (state) => {
+    console.log(state)
     return {songs: state.songs}
 }
 
-export default connect(mapStateToProps)(SongList)
+export default connect(mapStateToProps, {selectSong})(SongList)
